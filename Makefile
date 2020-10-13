ce9838a17504d2562d8fd0f7e9e2fa6ef1042ccf
pehcy <cheeyung.peh@gmail.com>
2020-10-13 14:25:39 +0800

    refactor: enhanced the structure

--- Makefile
+++ Makefile
@@ -1,10 +1,23 @@
-PYTHON = python3
+### MAIN TARGET ###
+.PHONY = virtual install build-requirements pfpdf2
+all: check test
+
+python = python3
+PYTHON := $(BIN)/$(python)
 project_name = pdf_merger
 
 virtual: venv/bin/pip
 
 .venv.bin/pip:
-			virtualenv -p /usr/bin/${PYTHON} .venv
+		virtualenv -p /usr/bin/${PYTHON} .venv
+
+# install PyPDF2 library
+.venv/bin/pypdf2:
+		.venv/bin/pip install -U PyPDF2
 
 run:
 			${PYTHON} ${project_name}.py
+
+clean:
+		rm -f MANIFEST
+		rm -rf build dist
--- pdf_merger.py
+++ pdf_merger.py
@@ -21,12 +21,14 @@ def merge_pdfs(files):
 def output_pdf(output_filename):
     merger.write(output_filename)
 
-# window = tk.Tk()
-# window.mainloop()
-
-# print("Hello, world")
-
 if __name__ == "__main__":
+    # create desktop GUI
     window = tk.Tk()
+    window.title("PDF Merger Tk")
+    window.rowconfigure(0, minsize=800, weight=1)
+    window.columnconfigure(1, minsize=800, weight=1)
+    
+    framed_btn = tk.Frame(window, relief=tk.RAISED, bd=2)
     btn_merge = tk.Button(master=window,text="Merge PDF", command=merge_pdfs)
+    btn_append = tk.Button()
     window.mainloop()
